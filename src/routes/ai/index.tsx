86b50869e380e76cb16400c30b795e193ef43a31
Brady Underwood <underwood.brady@gmail.com>
2024-05-20 02:33:33 -0600

    testing

---
+++ src/routes/ai/index.tsx
@@ -0,0 +1,26 @@
+import { component$ } from "@builder.io/qwik";
+import type { DocumentHead } from "@builder.io/qwik-city";
+import Ai from "~/components/UI/ai.jsx";
+import Software from "~/components/UI/software";
+import Footer from "~/components/main/footer/footer";
+
+/*import Counter from "~/components/starter/counter/counter";*/
+
+export default component$(() => {
+  return (
+    <>
+      <Ai/>
+      <Footer/>
+    </>
+  );
+});
+
+export const head: DocumentHead = {
+  title: "Brady Underwood | AI",
+  meta: [
+    {
+      name: "this is a description",
+      content: "this is a descripotion",
+    },
+  ],
+};
